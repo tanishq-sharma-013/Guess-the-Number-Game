e22a1a952979f4d6dff7f6a699a6352b6d5f27ea
tanishq-sharma-013 <163640580+tanishq-sharma-013@users.noreply.github.com>
2024-09-12 22:59:54 +0530

    Update guessTheNumber.java

--- guessTheNumber.java
+++ guessTheNumber.java
@@ -8,21 +8,29 @@ public class guessTheNumber {
         Random random = new Random();
         int numberToGuess = random.nextInt(101);
 
-        System.out.print("Guess a number between 0-100 : ");
+        System.out.print("Guess a number between 0-100( or '-1' to exit the game) : ");
 
         while (true) {
+            if(scanner.hasNextInt()){
+                int guessedNumber = scanner.nextInt();
             
-            int guessedNumber = scanner.nextInt();
+                if(guessedNumber==-1) {
+                    System.out.println("Exited from Game!! GOODBYE!!");
+                    break;
+                }
 
-            if (numberToGuess<guessedNumber) {
-                System.out.print("Guessed Number is too high!\nTry Again : ");
-            } else if (numberToGuess>guessedNumber) {
-                System.out.print("Guessed Number is too low!\nTry again : ");
+                if (numberToGuess<guessedNumber) {
+                    System.out.print("Guessed Number is too high!\nTry Again : ");
+                } else if (numberToGuess>guessedNumber) {
+                    System.out.print("Guessed Number is too low!\nTry again : ");
+                } else {
+                    System.out.println("Congratulation! You found the number!!\nThe number was : "+numberToGuess);
+                    break;
+                }
             } else {
-                System.out.println("Congratulation! You found the number!!\nThe number was : "+numberToGuess);
-                break;
+                System.out.print("Invalid Input!! Please enter a valid number(between 0-100) : ");
+                scanner.next();
             }
-            
         }
     }
     
